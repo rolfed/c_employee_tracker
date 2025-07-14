98a9a5af999df2e30658663d9ca0cc1101411c29
Danniel Rolfe <talkto@dannielrolfe.com>
2025-07-13 22:10:16 -0700

    Scaffold

--- src/main.c
+++ src/main.c
@@ -7,6 +7,14 @@
 // #include "parse.h"
 
 
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n - create new database file\n");
+    printf("\t -f - {required} path to database file\n");
+
+    return;
+}
+
 int main(int argc, char *argv[]) {
     char *filepath = NULL;
     bool newfile = false;
@@ -26,6 +34,12 @@ int main(int argc, char *argv[]) {
             default:
                 return -1;
         }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is required argument\n");
+        print_usage(argv);
+        return 0;
     }
 
     printf("Newfile: %d\n", newfile);
