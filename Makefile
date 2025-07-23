476734864bce5609b8303935ad3d53a036011146
Danniel Rolfe <danniel.rolfe@experian.com>
2025-07-23 11:38:11 -0700

    Add functionality to create new file, read from new file.

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
 
 run: clean default
 	./$(TARGET) -f ./mynewdb.db -n 
-	./$(TARGET) -f ./mynewdb.db -a "Timmy H.,123 Sheshire Ln.,120"
+	./$(TARGET) -f ./mynewdb.db  
 
 default: $(TARGET)
 
@@ -16,5 +16,5 @@ clean:
 $(TARGET): $(OBJ)
 	gcc -o $@ $?
 
-obj/%.o : src/%.c
+obj/%.o: src/%.c
 	gcc -c $< -o $@ -Iinclude
--- src/main.c
+++ src/main.c
@@ -1,49 +1,79 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-// #include "common.h"
-// #include "file.h"
-// #include "parse.h"
-
+#include "common.h"
+#include "file.h"
+#include "parse.h"
 
 void print_usage(char *argv[]) {
-    printf("Usage: %s -n -f <database file>\n", argv[0]);
-    printf("\t -n - create new database file\n");
-    printf("\t -f - {required} path to database file\n");
+  printf("Usage: %s -n -f <database file>\n", argv[0]);
+  printf("\t -n - create new database file\n");
+  printf("\t -f - {required} path to database file\n");
 
-    return;
+  return;
 }
 
 int main(int argc, char *argv[]) {
-    char *filepath = NULL;
-    bool newfile = false;
-    int c;
+  char *filepath = NULL;
+  bool newfile = false;
+  int c;
 
-    while ((c = getopt(argc, argv, "nf:")) != -1) {
-        switch (c) {
-            case 'n':
-                newfile = true;
-                break;
-            case 'f':
-                filepath = optarg;
-                break;
-            case '?':
-                printf("Unkown option -%c\n", c);
-                break;
-            default:
-                return -1;
-        }
+  int dbfd = -1;
+  struct dbheader_t *dbhdr = NULL;
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) {
+    switch (c) {
+    case 'n':
+      newfile = true;
+      break;
+    case 'f':
+      filepath = optarg;
+      break;
+    case '?':
+      printf("Unkown option -%c\n", c);
+      break;
+    default:
+      return -1;
     }
+  }
 
-    if (filepath == NULL) {
-        printf("Filepath is required argument\n");
-        print_usage(argv);
-        return 0;
-    }
-
-    printf("Newfile: %d\n", newfile);
-    printf("Filepath: %s\n", filepath);
-
+  if (filepath == NULL) {
+    printf("Filepath is required argument\n");
+    print_usage(argv);
     return 0;
+  }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to create database file\n");
+      return -1;
+    }
+
+    if (create_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+      printf("Failed to create database header\n");
+      return -1;
+    }
+
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to open database file\n");
+      return -1;
+    }
+
+    if (validate_db_header(dbfd, &dbhdr) == STATUS_ERROR) {
+      printf("Failed to validate database header\n");
+      return -1;
+    }
+  }
+
+  printf("Newfile: %d\n", newfile);
+  printf("Filepath: %s\n", filepath);
+
+  output_file(dbfd, dbhdr);
+
+  return 0;
 }
