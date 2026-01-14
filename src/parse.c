76ea639f7ebe40ac385c7005c0fe3e6d5ecbddd4
Danniel Rolfe <danniel.rolfe@experian.com>
2026-01-14 14:49:52 -0800

    Able to add employee to db

--- src/parse.c
+++ src/parse.c
@@ -60,16 +60,18 @@ int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) 
     return STATUS_ERROR;
   }
 
+  int realcount = dbhdr->count; 
+
   dbhdr->magic = htonl(dbhdr->magic);
-  dbhdr->filesize = htonl(dbhdr->filesize);
-  dbhdr->count = htons(dbhdr->filesize);
+  dbhdr->filesize = htonl(sizeof(struct dbheader_t) + (sizeof(struct employee_t) * realcount));
+  dbhdr->count = htons(dbhdr->count);
   dbhdr->version = htons(dbhdr->version);
 
   lseek(fd, 0, SEEK_SET);
   write(fd, dbhdr, sizeof(struct dbheader_t));
 
   int i = 0;
-  for(; i < dbhdr->count; i++) {
+  for(; i < realcount; i++) {
     employees[i].hours = htonl(employees[i].hours);
     write(fd, &employees[i], sizeof(struct employee_t));
   }
